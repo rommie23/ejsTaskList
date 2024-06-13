3d75cbd05a9cadd0c486fb244dc3a1047d1cebd3
Rohit Thakur <romi.thakur001@gmail.com>
2024-06-13 21:27:39 +0530

    first

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require("path")
+const fs = require("fs")
+
+const app = express()
+
+app.set("view engine", "ejs")
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/",(req, res)=>{
+    fs.readdir(`./files`, function(err, files){
+        // console.log(files);
+        res.render("index", {files:files})
+    })
+})
+
+app.get("/file/:filename",(req, res)=>{
+    fs.readFile(`./files/${req.params.filename}`,"utf-8", function(err, filedata){
+        // console.log(filedata);
+        res.render("show",{filename: req.params.filename, filedata: filedata})
+    })
+})
+
+app.post("/create",(req, res)=>{
+    // console.log(req.body);
+    fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`, `${req.body.description}`, function(err){
+        res.redirect("/")
+    })
+})
+
+app.listen(3000, ()=>{
+    console.log("server running");
+})
